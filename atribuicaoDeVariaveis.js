b77b6f7b3bbc6e193cc83918bf7b903519a7bd05
Raynara Vitoria Ferreira da Silva <raynaravitoria755@gmail.com>
2025-05-14 18:59:53 -0300

    Listas

--- atribuicaoDeVariaveis.js
+++ atribuicaoDeVariaveis.js
@@ -1,18 +1,23 @@
 console.log('Trabalhando com atribuições de Variáveis');
 
-const idade = 25;
+//const idade = 25;
 //const nome = 'Raynara';
-let nome = 'Raynara';
+let primeiroNome = 'Raynara';
 const sobrenome = 'Ferreira';
 
 //console.log(nome + ' ' + sobrenome); tanto desta forma quando com a virgula ambos irá dar um espaço entro os textos
-console.log(nome, sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
 //nome = nome + sobrenome; //está variável não pode ser sobrescrita, pois é uma constanta. Assim não podedo ser mudada.
 
 
-nome = nome + sobrenome; //irá sobrescrever o valor da variável nome
-console.log(nome);
+primeiroNome = primeiroNome + sobrenome; //irá sobrescrever o valor da variável nome
+console.log(primeiroNome);
+
+let idade; //declarando uma variável
 //não fazer 
+idade = 25; //atribuindo valor
+idade = idade+1;
+console.log(idade);
 //mes = 'Janeiro';
---
+++ listas.js
@@ -0,0 +1,16 @@
+console.log(`Trabalhando com listas `);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array (
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Belém`
+);
+
+listaDeDestinos.push(`Curitiba`); // adicionando um item na lista
+console.log('Destinos Possíveis:');
+console.log(listaDeDestinos);
+// console.log(salvador, saoPaulo, rioDeJaneiro);
